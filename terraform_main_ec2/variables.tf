bf52e20c93edf8ea84b5f7207b082be520f99ce9
Kesava0662 <164548091+Kesava0662@users.noreply.github.com>
2026-05-24 00:05:20 +0530

    Update variables.tf

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -64,7 +64,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0150ccaf51ab55a51" // Replace with the latest AMI ID for your region
+  default     = "ami-091138d0f0d41ff90" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
@@ -76,7 +76,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "us-east-1"
+  default     = "devops-key"
 }
 
 variable "instance_name" {
